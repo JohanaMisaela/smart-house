f5676f16c2b296a421812034cf5e521ac8ae2f37
JohanaMisaela <johanna.tahianaharison@gmail.com>
2024-05-28 22:19:57 +0300

    trano

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -9,9 +9,23 @@ const Page = () => {
   const { brightness } = useLightContext();
 
   return (
-    <div style={{ padding: "20px" }}>
-      <Lamp brightness={brightness} />
-    </div>
+    <>
+      <div className="fixed top-0 bottom-0 w-full h-full bg-green-300 ">
+        <div className=" w-full h-1/3 bg-blue-300 "></div>
+        <div className=" w-full h-3/5 mt-[-10%]">
+          <div className="w-3/4 mx-auto h-full border border-solid border-black grid grid-cols-2 grid-rows-2">
+            <div className="w-full h-full bg-white">
+              <div className="flex items-center justify-center w-full">
+                <Lamp brightness={brightness} />
+              </div>
+            </div>
+            <div className="w-full h-full"></div>
+            <div className="w-full h-full"></div>
+            <div className="w-full h-full"></div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
