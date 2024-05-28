4d6f921e96a02762f0bdb835efd1537208fff96a
JohanaMisaela <johanna.tahianaharison@gmail.com>
2024-05-28 22:06:13 +0300

    couleur lampe

--- client/src/devices/Lamp.tsx
+++ client/src/devices/Lamp.tsx
@@ -7,22 +7,38 @@ interface LampProps {
 const Lamp: React.FC<LampProps> = ({ brightness }) => {
   const getLampColor = (brightness: number): string => {
     const brightnessPercentage = brightness / 100;
-    const red = Math.floor(255 * (1 - brightnessPercentage));
-    const green = Math.floor(255 * (1 - brightnessPercentage));
-    const blue = 255;
+    let red, green, blue;
+    if (brightnessPercentage === 0) {
+      red = 0;
+      green = 0;
+      blue = 0;
+    } else if (brightnessPercentage === 1) {
+      red = 255;
+      green = 255;
+      blue = 255;
+    } else {
+      red = Math.floor(255 * brightnessPercentage);
+      green = Math.floor(255 * brightnessPercentage);
+      blue = Math.floor(255 * brightnessPercentage);
+    }
+    if (brightnessPercentage > 0.5) {
+      const yellowPercentage = (brightnessPercentage - 0.5) * 2;
+      red = 255;
+      green = 255;
+      blue = Math.floor(255 * (1 - yellowPercentage));
+    }
+
     return `rgb(${red}, ${green}, ${blue})`;
   };
 
   return (
     <div
       style={{
-        marginTop: "20px",
         width: "200px",
         height: "20px",
         borderBottomLeftRadius: 20,
         borderBottomRightRadius: 20,
         backgroundColor: getLampColor(brightness),
-        border: "1px solid #000",
         borderRadius: "10px",
       }}
     ></div>
