fc2509a0fd7181f275e7ddfbad97c1c0f8705b22
tolotraRan <ranarisontolotra@gmail.com>
2024-06-18 23:12:17 +0200

    mise en place de middleware pour proteger la route

---
+++ client/src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  console.log('Middleware invoked for path:', request.nextUrl.pathname);
+
+  if (request.nextUrl.pathname.startsWith('/interieur')) {
+    try {
+      const res = await fetch(`${request.nextUrl.origin}/api/door`);
+      const data = await res.json();
+      console.log('Door state:', data);
+
+      if (!data.isDoorOpen) {
+        console.log('Door is closed. Redirecting to /home');
+        return NextResponse.redirect(new URL('/home', request.url));
+      }
+    } catch (error) {
+      console.error('Failed to fetch door state:', error);
+      return NextResponse.redirect(new URL('/home', request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/interieur',
+};
